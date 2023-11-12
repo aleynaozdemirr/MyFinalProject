7dfb2073abed8f365d307a16e0e277a97f33cdf7
aleynaozdemirr <148920258+aleynaozdemirr@users.noreply.github.com>
2023-11-12 19:26:11 +0300

    Add files via upload

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{   
+    
+    //Generic Constraint
+    //Class : referans tip olduğunu gösterir.
+    //IEntity: IEntity olabilir veya IEntity implement eden bir nesne olabilir demektir.
+    //new(): newlenebilir olmalıdır.
+    public interface IEntityRepository<T> where T: class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
